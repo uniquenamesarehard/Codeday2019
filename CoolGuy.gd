525172e41b2ab76715d6b67b9f47f9becb88c6ae
Owen <43004995+UnintelliJ@users.noreply.github.com>
2019-02-16 14:58:30 -0500

    CHANGES BUT I GOT ANIME

--- CoolGuy.gd
+++ CoolGuy.gd
@@ -3,6 +3,13 @@ extends Sprite
 
 func _ready():
 	position = Vector2(get_viewport().size.x/2, get_viewport().size.y/2)
-	pass
-
-
+	scale = Vector2(0.3,0.3)
+	
+	rotate(deg2rad(90))
+	
+	set_process(true)
+	
+func _process(delta):
+	rotation = self.rotation + deg2rad(90*delta)
+	
+	translate(Vector2(-1000*delta,0))
