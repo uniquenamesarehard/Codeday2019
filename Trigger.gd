92c001e2d9bff5010962c3d8e663f62fe09fdc94
iris_li <irisli12345@gmail.com>
2019-02-17 04:01:56 -0500

    commit

--- Trigger.gd
+++ Trigger.gd
@@ -1,21 +1,21 @@
-extends Node
+extends Area2D
+signal triggered
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
 func _ready():
-	$Lightning.hide()
-	pass
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
-	#$Man.start($StartPosition.position)
+	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
 
-#func new_game():
-func lightning():
-	$Lightning.show()
+
+func _on_Trigger_body_entered(body):
+	emit_signal("triggered")
+	$Collision.disabled = true
