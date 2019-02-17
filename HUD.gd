6e208f011d921abee7e1be6854a40049e8a4dcea
iris_li <irisli12345@gmail.com>
2019-02-16 20:50:27 -0500

    Added some HUD stuff

---
+++ HUD.gd
@@ -0,0 +1,31 @@
+extends CanvasLayer
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var text
+var blueprint_menu
+
+func _ready():
+	blueprint_menu = get_node("Blueprint")
+	$Panel.hide()
+	$MenuButton.show()
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+
+func _process(delta):
+	if Input.is_action_pressed("ui_select"): #and object is interactable
+		#$ExamineText.text = text
+		$Panel/ExamineText.text = "Today is February 16, 2019. This is the day we build a game"
+		$Panel.show()
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_MenuButton_pressed():
+	blueprint_menu.show()
+
+func _on_closeButton_pressed():
+	blueprint_menu.hide()
+	
