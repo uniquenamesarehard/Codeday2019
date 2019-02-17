1f8c78d5aba6ddb525b7e68ace4554bd10b7497e
uniquenamesarehard <31969760+uniquenamesarehard@users.noreply.github.com>
2019-02-17 10:06:37 -0500

    ahaha

---
+++ Portal.gd
@@ -0,0 +1,18 @@
+extends Area2D
+signal p
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+func _on_Area2D_body_entered(body):
+	emit_signal("p")
+	
