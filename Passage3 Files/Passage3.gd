e95ab9d3daff5bb83a2008359c3dedc54660b7cd
iris_li <irisli12345@gmail.com>
2019-02-17 05:29:02 -0500

    commit

--- Passage3 Files/Passage3.gd
+++ Passage3 Files/Passage3.gd
@@ -1,11 +1,17 @@
-extends Area2D
-signal lightning
+extends Node
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
 
 func _ready():
-	pass
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+func lightning():
+	print("hello")
